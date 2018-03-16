74d349cdc5a186507dc4518b262969f462b7207e
Manuel Navarro Pérez <manuelnavarro1987@gmail.com>
2018-03-16 13:23:50 +0100

    New repository chip component

---
+++ src/components/presentationals/RepositoryChip.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  v-chip(color="orange" text-color="white")
+    v-icon.mr-2 check_circle
+    | {{ repository.name }}
+</template>
+
+<script>
+export default {
+  name: 'RepositoryChip',
+  props: {
+    repository: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
