d1bae38465546e8b5f83a3fc9be717be2379afa5
Manuel Navarro Pérez <manuelnavarro1987@gmail.com>
2018-03-21 14:16:12 +0100

    Upload static build to S3

--- poi.config.js
+++ poi.config.js
@@ -1,3 +1,5 @@
+const S3Plugin = require('webpack-s3-plugin')
+
 module.exports = options => ({
   html: {
     template: './src/index.html',
@@ -9,4 +11,21 @@ module.exports = options => ({
     require('poi-preset-yaml')(),
     require('poi-preset-uglifyjs')(),
   ],
+  webpack: (config, options, webpack) => {
+    if (process.env.PUBLISH_S3 === 'true') {
+      config.plugins.push(
+        new S3Plugin({
+          s3Options: {
+            accessKeyId: process.env.S3_KEY,
+            secretAccessKey: process.env.S3_SECRET,
+            region: process.env.S3_REGION,
+          },
+          s3UploadOptions: {
+            Bucket: process.env.S3_BUCKET,
+          },
+        })
+      )
+    }
+    return config
+  },
 })
